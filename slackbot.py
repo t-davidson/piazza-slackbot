ca91ed36397dbdd973b93317d237f5cdbef9ba9b
Tom Davidson <trd54@cornell.edu>
2017-01-27 13:29:59 -0500

    Added code to run a basic slackbot

---
+++ slackbot.py
@@ -0,0 +1,48 @@
+"""
+This is a simple Slackbot that will
+check a Piazza page for new posts every 1 minute.
+Every time a new post is observed a notification will
+be sent out
+"""
+
+from piazza_api import Piazza
+from slacker import Slacker
+from time import sleep
+
+#Accessing Piazza and loading data
+piazza_id = "" #this is the suffix of the piazza url
+piazza_email = "" #your email
+piazza_password = "" #your piazza piazza_password
+p = Piazza()
+p.user_login(email=piazza_email, password=piazza_password)
+network = p.network(piazza_id)
+
+#Accessing Slack and configuring the bot
+slack_token = ""
+bot=Slacker(slack_token) #authorizing bot
+channel='piazza' #Name of the channel to post to
+bot_name = '' #Name of your slackbot
+message="New message on Piazza!"#Message to send
+
+def check_for_new_posts(NUM_POSTS):
+    """
+    This function will run continuously,
+    checking every 1 minute to see if the number of posts
+    in the Piazza feed is equal to the previous number.
+    If a new post is added then a message will be sent to
+    Slack.
+    """
+    while True:
+        feed = network.search_feed('')
+        if len(feed) > NUM_POSTS:
+            bot.chat.post_message(channel,message, \
+            as_user=bot_name,parse='full')
+            NUM_POSTS = len(feed)
+        else:
+            pass
+        print("Slackbot is running...")
+        sleep(60)
+
+if __name__ == '__main__':
+     NUM_POSTS = len(network.search_feed(''))
+     check_for_new_posts(NUM_POSTS)
