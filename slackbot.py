d41ba72acf81629f9f34c228b5d1a52ac9f514dc
Teddy Heidmann <ateddy42@users.noreply.github.com>
2017-01-31 11:13:49 -0500

    Changed tabs to spaces

--- slackbot.py
+++ slackbot.py
@@ -27,10 +27,10 @@ bot_name = "" #TODO Name of your slackbot
 POST_BASE_URL = "https://piazza.com/class/"+piazza_id+"?cid="
 
 def get_max_id(feed):
-	for post in feed:
-		if "pin" not in post:
-			return post["nr"]
-	return -1
+    for post in feed:
+        if "pin" not in post:
+            return post["nr"]
+    return -1
 
 def check_for_new_posts(LAST_ID,include_link=True):
     """
@@ -41,18 +41,18 @@ def check_for_new_posts(LAST_ID,include_link=True):
     Slack.
     """
     while True:
-		UPDATED_LAST_ID = get_max_id(network.get_feed()['feed'])
+        UPDATED_LAST_ID = get_max_id(network.get_feed()['feed'])
         if UPDATED_LAST_ID > LAST_ID:
             attachment = None
             message = None
             if include_link is True:
                 attachment = [
                     {
-			"fallback": "New post on Piazza!",
+                        "fallback": "New post on Piazza!",
                         "title": "New post on Piazza!",
                         "title_link": POST_BASE_URL+str(UPDATED_LAST_ID),
                         "text": "Follow the link to view this post",
-			            "color": "good"
+                        "color": "good"
                     }
                 ]
             else:
@@ -66,5 +66,5 @@ def check_for_new_posts(LAST_ID,include_link=True):
         sleep(60)
 
 if __name__ == '__main__':
-	LAST_ID = get_max_id(network.get_feed()['feed'])
-	check_for_new_posts(LAST_ID)
+    LAST_ID = get_max_id(network.get_feed()['feed'])
+    check_for_new_posts(LAST_ID)
